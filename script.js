1a40718924c367cb8e953564925d17395a05b47b
Andrey Rastegaev <embrion83@yandex.ru>
2020-07-04 10:59:33 +0300

    исправить js

--- script.js
+++ script.js
@@ -8,20 +8,23 @@ let jobInput = popup.querySelector('.popup__input_type_job');
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
 
-function togglePopup() {
-    popup.classList.toggle('popup_is-open');
+function popupIsOpen() {
+    popup.classList.add('popup_is-open');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
 
-openPopup.addEventListener ('click', togglePopup);
-closePopup.addEventListener ('click', togglePopup);
+function popupIsClosed() {
+    popup.classList.remove('popup_is-open');
+}
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    popup.classList.remove('popup_is-open');
+    popupIsClosed();
 }
 
+openPopup.addEventListener ('click', popupIsOpen);
+closePopup.addEventListener ('click', popupIsClosed);
 formElement.addEventListener ('submit', formSubmitHandler);
